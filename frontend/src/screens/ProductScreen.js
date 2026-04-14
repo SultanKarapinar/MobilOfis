b8aaf5f83761f6014d8b94224b8069e314381ef8
SultanKarapinar <sultankrpnr04@gmail.com>
2026-04-14 16:52:51 +0300

    ürünler sayfası guzellestırıldı

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,8 +9,8 @@ const ProductScreen = ()=>{
 
     const [products , setProducts]=useState([
         {id:1,name:'Çay',category:'Mutfak', CurrentStock:2,ReorderLevel:5},
-        {id:2,name:'Kahve',category:'Mutfak', CurrentStock:15,Reorder:10 },
-        {id:3,name:'Şeker',category:'Mutfak',CurrentStock:20,ReorderLevel:5},
+        {id:2,name:'Kahve',category:'Mutfak', CurrentStock:15,ReorderLevel:10},
+        {id:3,name:'Şeker',category:'Mutfak',CurrentStock:20,ReorderLevel:12},
     ]);
 
     const filteredProducts=filter==='low' ? products.filter(p=> p.CurrentStock <= p.ReorderLevel) : products;
@@ -33,8 +33,7 @@ const ProductScreen = ()=>{
             {filteredProducts.map((item)=>(
                 <Card key={item.id} style={styles.card}>
                     <List.Accordion //acılır liste 
-                    title={item.name}
-                    description={`Kategori:${item.category}` }
+                    title={item.name} 
                     left={props => <List.Icon{...props} icon="package-variant-closed"/>}
                     right={()=>(
                         <Badge //bildirim sayısı gostermek daire içinde sayı
@@ -42,11 +41,25 @@ const ProductScreen = ()=>{
                         size={24}> {item.CurrentStock}</Badge>
                     )}>
                         <View style={styles.details}>
-                            <List.Item title="Ürün Adı" description={item.name} compact />
-                            <List.Item title ="Min Stok" description={item.ReorderLevel} compact />
+                        <View style={styles.row}>
+                        <Text style={styles.label}>Kategori</Text>
+                        <Text style={styles.value}>{item.category}</Text>
+                        </View>
+
+                        <View style={styles.row}>
+                         <Text style={styles.label}>Güncel Stok</Text>
+                         <Text style={styles.value}>{item.CurrentStock}</Text>
+                         </View>
+
+                        <View style={styles.row}>
+                        <Text style={styles.label}>Min Stok</Text>
+                        <Text style={styles.value}>{item.ReorderLevel}</Text>
+                        </View>
                             <View style ={styles.actionButtons}>
-                                <Button icon="pencil" mode="text" onPress={()=>{}}>Düzenle</Button>
-                                <Button icon ="trash-can" mode="text" color="red" onPrenss={()=>{}}>Sil</Button>
+                                <Button style={styles.button} icon="pencil" mode="contained" onPress={()=>{}}>Düzenle</Button>
+                                <Button style={styles.button} icon ="trash-can" mode="contained"  onPress={()=>{}}>Sil</Button>
+                                <Button style={styles.button} icon="plus" mode="contained" onPress={()=>{}}>Stok Arttır</Button>
+                                <Button style={styles.button} icon="minus" mode="contained" onPress={()=>{}}>Stok Azalt</Button>
                             </View>
                         </View>
                     </List.Accordion>
@@ -78,6 +91,7 @@ const styles= StyleSheet.create({
         flex:1,
         marginHorizontal:5,
         backgroundColor:'#98ad80',
+        color:'#ffff',
     },
     scrollContainer:{
         padding:10,
@@ -85,13 +99,35 @@ const styles= StyleSheet.create({
     card:{
         marginBottom:10,
     },
-    details:{
-        padding:10,
-        backgroundColor:'#fafafa',
-    },
+   details: {
+  padding: 12,
+  gap: 10,
+},
+
+row: {
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  },
+
+label: {
+  color: '#666',
+  fontSize: 14,
+},
+
+value: {
+  fontWeight: 'bold',
+  fontSize: 15,
+},
     actionButtons:{
         flexDirection:'row',
-        justifyContent:'flex-end'
+        flexWrap: 'wrap',   
+        justifyContent:'space-evenly',
+        
+    },
+    button:{
+      width:'48%',
+      marginBottom: 8,
+     backgroundColor:"#8b80b4",
     },
     fab:{
         pasition:'absolute',
